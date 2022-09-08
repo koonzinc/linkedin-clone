a655db99869f79c106afab5ba8c75480e60f323a
koonzinc <koonz.eth@gmail.com>
2022-09-07 21:01:02 -0400

    materialui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a linkedin clone</h1>
+
+      {/* Header */}
+      <Header />
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+        
     </div>
   );
 }
