cbe6976bac4fdf03c5c77fcfa1a2d24865ba02c8
koonzinc <koonz.eth@gmail.com>
2022-09-08 13:04:23 -0400

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './style/Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import profile from '../assets/profile.png';
 
 const Header = () => {
     return (
@@ -14,7 +21,12 @@ const Header = () => {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="My Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar={profile} title="me" />
             </div>
         </div>
     )
--- src/components/HeaderOption.jsx
+++ src/components/HeaderOption.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './style/HeaderOption.css';
+import { Avatar } from '@mui/material';
 
-const HeaderOption = () => {
-  return (
-    <div>HeaderOption</div>
-  )
+const HeaderOption = ({ avatar, Icon, title }) => {
+    return (
+        <div className='headerOption'>
+            {Icon && <Icon className="headerOption__icon" />}
+            {avatar && (
+                <Avatar className='headerOption__icon' src={avatar} />
+            )}
+            <h3 className='headerOption__title'>{title}</h3>
+        </div>
+    )
 }
 
 export default HeaderOption
