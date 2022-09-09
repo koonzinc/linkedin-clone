128ff42117754012dae9032846f41fa8841e30c6
koonzinc <koonz.eth@gmail.com>
2022-09-09 18:01:03 -0400

    all

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style/Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
@@ -6,28 +6,39 @@ import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
 
 const Feed = () => {
-  return (
-    <div className='feed'>
-        <div className='feed__inputContainer'>
-            <div className='feed__input'>
-                <CreateIcon />
-                <form>
-                    <input type="text" />
-                    <button type='submit'>Send</button>
-                </form>
+    const [posts, setPosts] = useState([])
+
+    const sendPost = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className='feed'>
+            <div className='feed__inputContainer'>
+                <div className='feed__input'>
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button onClick={sendPost} type='submit'>Send</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
+                </div>
             </div>
-            <div className="feed__inputOptions">
-                <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
-                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
-                <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD" />
-                <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
-            </div>
+            {/* Posts */}
+            {posts.map((post) => (
+                <Post />
+            ))}
+            <Post name='William Koonz' description='This is a test' message='WOW this worked' />
         </div>
-        {/* Posts */}
-    </div>
-  )
+    )
 }
 
 export default Feed
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQaaisLjuVI6-qhA2AfzjIAbMUlV4Uhhc",
+    authDomain: "linkedin-2e6ad.firebaseapp.com",
+    projectId: "linkedin-2e6ad",
+    storageBucket: "linkedin-2e6ad.appspot.com",
+    messagingSenderId: "113191366387",
+    appId: "1:113191366387:web:b6e8fd970e178a63d5d186"
+  };
