c7e11ce9cdf8e2879506df759b8c9567a69c2a88
koonzinc <koonz.eth@gmail.com>
2022-09-12 14:55:28 -0400

    all

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widgets from './components/Widgets';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './components/Login';
 
 function App() {
+const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
 
     </div>
   );
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Login.css';
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <h1>You are not logged in</h1>
+    </div>
+  )
+}
+
+export default Login
