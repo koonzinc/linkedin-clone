51963e87cf9de59633699b4abaf35bd4c61b1d37
koonzinc <koonz.eth@gmail.com>
2022-09-12 12:55:28 -0400

    all

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCQaaisLjuVI6-qhA2AfzjIAbMUlV4Uhhc",
     authDomain: "linkedin-2e6ad.firebaseapp.com",
@@ -6,3 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "113191366387",
     appId: "1:113191366387:web:b6e8fd970e178a63d5d186"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
